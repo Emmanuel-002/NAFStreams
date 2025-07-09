4ea15cb115fab70f1100cccc15922d6dac9cb876
Emmanuel-002 <segseyman8@gmail.com>
2025-07-09 18:55:00 +0100

    second ui adjustment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -33,6 +33,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../Frontend/app")));
 
   app.get("*", (req, res) => {
+    
     res.sendFile(path.join(__dirname, "../Frontend", "app", "index.html"));
   });
 }
