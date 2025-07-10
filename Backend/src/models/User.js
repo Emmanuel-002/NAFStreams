2e0f404fe871cafd8f3a56c751371279a0d0d194
Emmanuel-002 <segseyman8@gmail.com>
2025-07-10 11:15:07 +0100

    adjust menu to contact registry

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -41,10 +41,10 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
-    isVerified: {
-      type: Boolean,
-      default: false,
-    },
+    // isVerified: {
+    //   type: Boolean,
+    //   default: false,
+    // },
     friends: [
       {
         type: mongoose.Schema.Types.ObjectId,
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 import { Link, useLocation } from "react-router";
 import useAuthUser from "../hooks/useAuthUser";
-import { BellIcon, LogOutIcon, Scroll, UsersIcon, Video } from "lucide-react";
+import { BellIcon, Contact, LogOutIcon, Menu, PaletteIcon, Scroll, UsersIcon, Video } from "lucide-react";
 import ThemeSelector from "./ThemeSelector";
 import useLogout from "../hooks/useLogout";
+import { THEMES } from "../constants";
+import { useThemeStore } from "../store/useThemeStore";
+import { useQuery } from "@tanstack/react-query";
+import { getUserFriends } from "../lib/api";
+import NoFriendsFound from "./NoFriendsFound";
+import FriendCard from "./FriendCard";
 
 const Navbar = () => {
+  const { theme, setTheme } = useThemeStore();
   const { authUser } = useAuthUser();
   const location = useLocation();
   const currentPath = location.pathname;
   const isChatPage = location.pathname?.startsWith("/chat");
+
+  const { data: friends = [], isLoading: loadingFriends } = useQuery({
+    queryKey: ["friends"],
+    queryFn: getUserFriends,
+  });
 
   const handleClick = () => {
     const elem = document.activeElement;
@@ -28,60 +40,38 @@ const Navbar = () => {
   return (
     <div class="navbar bg-base-100 shadow-sm">
   <div class="navbar-start">
-    <div class="dropdown z-30">
-      <div tabindex="1" role="button" class="btn btn-ghost btn-circle">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-5 w-5 stroke-current"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
-        {/* <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /> </svg> */}
-      </div>
-      <ul
-        tabindex="0"
-        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li onClick={handleClick}>
-          <Link
-                    to="/community"
-                    className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${
-                      currentPath === "/community" ? "btn-active" : ""
-                    }`}
-                  >
-                    <UsersIcon className="size-5 text-base-content opacity-70" />
-                    <span>Community</span>
-                  </Link>
-        </li>
-        <li onClick={handleClick}>
-          <Link
-                    to="/about"
-                    className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${
-                      currentPath === "/about" ? "btn-active" : ""
-                    }`}
-                  >
-                    <Scroll className="size-5 text-base-content opacity-70" />
-                    <span>About</span>
-                  </Link>
-        </li>
-        <li onClick={handleClick}>
-            <Link
-                    to="/profile"
-                  >
-                    <div className="p-4 border-t border-base-300 mt-auto">
-        <div className="flex items-center gap-3">
-          <div className="avatar">
-            <div className="w-10 rounded-full">
-              <img src={authUser?.profilePic} alt="User Avatar" />
+    <div className="dropdown z-30">
+          <button tabIndex={0} className="btn btn-ghost btn-circle">
+            <Menu className="size-9" />
+          </button>
+    
+          <div
+            tabIndex={0}
+            className="dropdown-content mt-2 p-1 shadow-2xl bg-base-200 backdrop-blur-lg rounded-2xl
+            w-56 border border-base-content/10 max-h-80 overflow-y-auto"
+          >
+            <div className="space-y-1">
+                <button
+                  key={1}
+                  className={`w-full px-4 py-3 rounded-xl flex items-center gap-3 transition-colors`}
+                  onClick={() => null }
+                >
+                  <div className="ml-auto flex gap-1">
+                    {friends.length === 0 ? (
+                      <p>No Connection Yet</p>
+                    ) : (
+                      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-2 m-4">
+                        {friends.map((friend) => (
+                          <FriendCard key={friend._id} friend={friend} />
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                </button>
+           
             </div>
           </div>
-          <div className="flex-1">
-            <p className="font-semibold text-sm">{authUser?.fullName}</p>
-            <p className="text-xs text-success flex items-center gap-1">
-              <span className="size-2 rounded-full bg-success inline-block" />
-                Online
-            </p>
-          </div>
         </div>
-      </div>
-                  </Link>
-        </li>
-      </ul>
-    </div>
   </div>
   <div class="navbar-center">
     <Link to="/" className="flex items-center gap-1">
@@ -109,40 +99,6 @@ const Navbar = () => {
           </button>
   </div>
 </div>
-    // <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center">
-    //   <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-    //     <div className="flex items-center justify-between w-full">
-    //       {/* LOGO - ONLY IN THE CHAT PAGE */}
-    //       {/* {isChatPage && ( */}
-    //         <div className="pl-1">
-              
-    //         </div>
-    //       {/* )} */}
-
-    //       <div className="flex items-center gap-3 sm:gap-4 ml-auto">
-    //         <Link to={"/notifications"}>
-    //           <button className="btn btn-ghost btn-circle">
-    //             <BellIcon className="h-6 w-6 text-base-content opacity-70" />
-    //           </button>
-    //         </Link>
-    //       </div>
-
-    //       {/* TODO */}
-    //       <ThemeSelector />
-
-    //       <div className="avatar">
-    //         <div className="w-9 rounded-full">
-    //           <img src={authUser?.profilePic} alt="User Avatar" rel="noreferrer" />
-    //         </div>
-    //       </div>
-
-    //       {/* Logout button */}
-    //       <button className="btn btn-ghost btn-circle" onClick={logoutMutation}>
-    //         <LogOutIcon className="h-6 w-6 text-base-content opacity-70" />
-    //       </button>
-    //     </div>
-    //   </div>
-    // </nav>
   );
 };
 export default Navbar;
